23aa9cab7f184e1ebfc54168a8438a290ccaa7d7
James White <cyberyt@gmail.com>
2015-12-01 20:34:06 +0000

    functionality is there, even though it looks pretty awful!

--- js/listingCtrl.js
+++ js/listingCtrl.js
@@ -11,37 +11,46 @@ function listingController($http) {
   var programmes = [];
   self.programmes = programmes;
 
-  var selectedLetter = '';
+  self.selectedLetter = 'a';
 
   // Paginate setup
   var pageCount;
+  self.pages = []
   var selectedPage = 1;
 
   // Get listings from API based on selection
+  self.getListings = getListings;
   function getListings(letter, page) {
-    console.log('getting listings')
+    console.log('getting listings for letter: '+letter+' and page: '+page)
+    self.selectedLetter = letter;
     $http
-      .get('http://ibl.api.bbci.co.uk/ibl/v1/atoz/'+ letter + '/programmes?page=' + page)
+      .get('http://ibl.api.bbci.co.uk/ibl/v1/atoz/'+ self.selectedLetter + '/programmes?page=' + page)
       .then(function(response) {
         console.log(response)
         processListings(response);
-        console.log(pageCount)
+        console.log('pageCount: ', pageCount)
       });
   }; 
 
   // Initial get defaults to 'A: Page1' if no selection exists.
-  getListings(selectedLetter || 'a', selectedPage || 1);
+  getListings(self.selectedLetter || 'a', selectedPage || 1);
 
   function processListings(response) {
     var resultsPerPage = 20; // for easy change in future.
     // below will give the pageCount variable a number to work with.
+    pageCount = 0;
+    self.pages = [];
     pageCount = Math.ceil(response.data.atoz_programmes.count / resultsPerPage);
+    for (var i=1; i<=pageCount; i++) {
+      self.pages.push(i);
+    }
+    // below build an array of program data objects.
     self.programmes = response.data.atoz_programmes.elements;
+
   }
 
   self.imageChef = imageChef;
   function imageChef(url) {
-    console.log(url)
     return url.replace('{recipe}', '406x228')
   };
 
