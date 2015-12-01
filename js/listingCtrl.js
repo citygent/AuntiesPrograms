2d4b3d6aa0afeba98fe19744a55a1a513fd4c785
James White <cyberyt@gmail.com>
2015-12-01 14:46:41 +0000

    got images and titles for hardcoded, lost some time on this imageChef thing

--- js/listingCtrl.js
+++ js/listingCtrl.js
@@ -2,19 +2,49 @@ angular.module('listApp', []).controller('listingController', listingController)
 listingController.$inject = ['$http'];
 
 function listingController($http) {
-  console.log('hello!')
-  this.title = "eRadioTimes";
   var self = this;
+  self.title = 'eRadioTimes';
 
+  // Alphabetized Nav setup
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  self.letters = alphabet.split('');
+  var programmes = [];
+  self.programmes = programmes;
+
+  var selectedLetter = '';
+
+  // Paginate setup
+  var pageCount;
+  var selectedPage = 1;
+
+  // Get listings from API based on selection
   function getListings(letter, page) {
     console.log('getting listings')
     $http
-      .get('http://ibl.api.bbci.co.uk/ibl/v1/atoz/a/programmes?page=1')
+      .get('http://ibl.api.bbci.co.uk/ibl/v1/atoz/'+ letter + '/programmes?page=' + page)
       .then(function(response) {
-        console.log(response);
+        console.log(response)
+        processListings(response);
+        console.log(pageCount)
       });
+  }; 
+
+  // Initial get defaults to 'A: Page1' if no selection exists.
+  getListings(selectedLetter || 'a', selectedPage || 1);
+
+  function processListings(response) {
+    var resultsPerPage = 20; // for easy change in future.
+    // below will give the pageCount variable a number to work with.
+    pageCount = Math.ceil(response.data.atoz_programmes.count / resultsPerPage);
+    self.programmes = response.data.atoz_programmes.elements;
+  }
+
+  self.imageChef = imageChef;
+  function imageChef(url) {
+    console.log(url)
+    return url.replace('{recipe}', '406x228')
   };
-  getListings();
+
 }
 
 // The API end point follows the following format:
